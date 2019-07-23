37a2cc6b6926267675297c58c7cc32aa1e15b07a
Ankit Gupta <guptas08@gmail.com>
2019-07-23 16:35:23 +0530

    Add files via upload

---
+++ loopy.java
@@ -0,0 +1,19 @@
+public class loopy{
+  
+	public static void main(string[] args) {
+
+ int x = 1;
+  
+ System.out.println("Before the loop");
+ while(x<4){
+   System.out.println("In the loop");
+   
+   System.out.println("value of x is " +x);
+   
+    x =  x + 1;
+
+} 
+ System.out.println("this is after loop");
+}
+
+}
